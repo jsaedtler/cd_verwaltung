88bbbaeae77eecf75479a9e6767a373465884663
Jörg Sädtler <j.saedtler@gmail.com>
2011-05-08 15:37:41 +0200

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CdVerwaltung::Application.config.secret_token = '4911a6fef3be4cdeca70025bf3c1703ae0cb09af54571e9fce9fa542ac4069c2d2b12c4285bcc291f5b1d828e98202a392f4d52f3527f592831605142c801007'
